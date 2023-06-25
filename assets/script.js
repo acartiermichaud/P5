4a9c5a9db68ce60220517a0de68b3b718863acd9
acartiermichaud <a.cartier.michaud@gmail.com>
2023-06-25 23:24:04 +0200

    ETAPE 4 : Modification du slide au clic sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -18,26 +18,90 @@ const slides = [
 ]
 
 
-// EventListeners on arrows
+
+// Functions *****************************************
+
+// Bullet points creation
+function dotsCreation (slideList) {
+
+	const divDots = document.querySelector(".dots")
+
+	for (let i = 0; i < slideList.length; i++) {
+		let selectedState = false
+		const dotElement = document.createElement("div")
+		dotElement.classList.add("dot")
+		dotElement.setAttribute("id", "dot_"+i)
+		
+		if (i===0) {
+			selectedState = true
+			dotElement.classList.add("dot_selected")
+		}
+
+		dotsList.push(selectedState)
+		divDots.appendChild(dotElement)
+	}
+}
+
+// Calculation of new index
+function newIndexCalculation (direction) {
+
+	for (let i = 0; i < dotsList.length; i++) {
+		
+		if (dotsList[i]) {
+			dotsList[i] = false
+
+			if (direction==="right") {
+				dotsList[i+1] = true
+				return [i, i+1]
+			}
+			else {
+				dotsList[i-1] = true
+				return [i, i-1]
+			}
+		}
+	}
+}
+
+// Dots update
+function dotsUpdate (oldPos, newPos) {
+	let oldDot = document.querySelector(".dot_selected")
+	oldDot.classList.remove("dot_selected")
+	
+	let newDot = document.getElementById("dot_"+newPos)
+	newDot.classList.add("dot_selected")
+}
+
+// Image update
+function imageUpdate (newPos) {
+	let imageContener = document.querySelector(".banner-img")
+	imageContener.src = `/home/amandine/Documents/OpenClassRooms/FormationIntegrateurWeb/Projet_5/P5_GitHub/P5/assets/images/slideshow/${slides[newPos].image}`
+	let imageText = document.querySelector(".banner-txt")
+	imageText.innerHTML = `<p>${slides[newPos].tagLine}</p>`
+}
+
+
+
+// Main *****************************************
+let dotsList = []
+dotsCreation(slides)
+
+// EventListener on arrow left
 const arrowLeft = document.querySelector(".arrow_left")
 arrowLeft.addEventListener ("click", () => {
-	console.log("Vous avez cliqué sur le bouton gauche.")
+	positions = newIndexCalculation("left")
+	oldPos = positions[0]
+	newPos = positions[1]
+	dotsUpdate (oldPos, newPos)
+	imageUpdate (newPos)
 })
 
+// EventListener on arrow right
 const arrowRight = document.querySelector(".arrow_right")
 arrowRight.addEventListener ("click", () => {
-	console.log("Vous avez cliqué sur le bouton droit.")
+	positions = newIndexCalculation("right")
+	oldPos = positions[0]
+	newPos = positions[1]
+	dotsUpdate (oldPos, newPos)
+	imageUpdate (newPos)
 })
 
-
-// Bullet points
-const divDots = document.querySelector(".dots")
-
-for (let i = 0; i < slides.length; i++) {
-	const dotElement = document.createElement("div")
-	dotElement.classList.add("dot")
-	if (i===0) {
-		dotElement.classList.add("dot_selected")
-	}
-	divDots.appendChild(dotElement)
-} 
